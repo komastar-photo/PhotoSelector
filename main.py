b0d08abd8f4a3653c9a2a37d81a977fd795b1eba
Eugene Kim <komastar.dev@gmail.com>
2023-09-16 12:04:13 +0900

    init project

---
+++ main.py
@@ -0,0 +1,5 @@
+from select_from_text import SelectFromText
+
+if __name__ == '__main__':
+    SelectFromText.select('H:/Photography/Portrait/2023/230902 다다님/select.txt',
+                          'H:/Photography/Portrait/2023/230902 다다님/C')
